603dad4f5ab6c1c8774b878074d4cc417f8863b7
Rana Umair <rjee4631@gmail.com>
2022-08-12 17:58:19 +0500

    Update ip.js

--- ip.js
+++ ip.js
@@ -8,3 +8,16 @@ fetchMoviesJSON().then(
         console.log(ips);
     }
 );
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+async function fetchMoviesJSON() {
+  const response = await fetch('https://api.ipify.org?format=json');
+  const ips = await response.json();  return ip;
+}
+
+var ip__ = await fetchMoviesJSON().then(
+    (ips)=>{
+        return (ips.ip);
+    });
+console.log(ip__);
